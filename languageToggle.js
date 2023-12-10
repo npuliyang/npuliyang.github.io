140ba86d30ded43df10589a594891c1f41b28baf
npuliyang <704155622@qq.com>
2023-12-10 16:53:41 +0800

    update

---
+++ languageToggle.js
@@ -0,0 +1,10 @@
+function switchLanguage(lang) {
+    var allElements = document.getElementsByClassName('lang');
+    for (var i = 0; i < allElements.length; i++) {
+        allElements[i].style.display = 'none';
+    }
+    var langElements = document.querySelectorAll('[id^="' + lang + '"]');
+    for (var i = 0; i < langElements.length; i++) {
+        langElements[i].style.display = 'block';
+    }
+}
